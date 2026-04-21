9e37db969502e96935b9433285d6130c4e77ba80
Shemdoe <georgehmariki@gmail.com>
2026-04-22 00:08:40 +0300

    done

--- utils/leagueCatalog.js
+++ utils/leagueCatalog.js
@@ -4,5 +4,6 @@ module.exports = [
   { leagueId: 140, displayOrder: 3 },
   { leagueId: 135, displayOrder: 4 },
   { leagueId: 78, displayOrder: 5 },
-  { leagueId: 61, displayOrder: 6 }
+  { leagueId: 61, displayOrder: 6 },
+  { leagueId: 88, displayOrder: 7 }
 ];
